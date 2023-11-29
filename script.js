f741a549346f2b26a21124273fa185299421610b
ki3b <152044232+ki3b@users.noreply.github.com>
2023-11-29 22:32:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+const navbar = document.querySelector('.navbar');
+const menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.addEventListener('click', ()=>{
+    navbar.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(navbar) && !e.composedPath().includes(menuBtn) ){
+            navbar.classList.remove('active');
+        }
+    });
+});
+
+
+
+
+
